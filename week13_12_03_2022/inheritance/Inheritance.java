cb9a06b6907d1b851829e17b3e93e190a2a93976
vahap iren <vahapiren@gmail.com>
2022-12-03 23:10:31 +0300

    week13: rewiew

--- week13_12_03_2022/inheritance/Inheritance.java
+++ week13_12_03_2022/inheritance/Inheritance.java
@@ -4,4 +4,5 @@ public class Inheritance {
     /*
     relationship between classes
      */
+
 }
---
+++ week13_12_03_2022/inheritance/Test.java
@@ -0,0 +1,10 @@
+package week13_12_03_2022.inheritance;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
