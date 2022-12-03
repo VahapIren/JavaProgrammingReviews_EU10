f0d64d9fc740d7781ecd0fb67fec2419255a1da3
vahap iren <vahapiren@gmail.com>
2022-12-03 20:10:20 +0300

    week13: rewiew

---
+++ week13_12_03_2022/GarbageCollection.java
@@ -0,0 +1,17 @@
+package week13_12_03_2022;
+
+public class GarbageCollection {
+
+    public static void main(String[] args) {
+        //how can we make the object unreferenced
+
+        //1-make it null
+        String str="Mesut";
+
+        //2-assign another value
+        String str1="Adam";
+        str1="Osman";
+
+
+    }
+}
--- week13_12_03_2022/warmup/School.java
+++ week13_12_03_2022/warmup/School.java
@@ -14,14 +14,69 @@ public class School {
         Teacher teacher3=new Teacher("Asyun","Abc",'F', LocalDate.of(1967,1,1),3L);
         Teacher teacher4=new Teacher("Mike","Abc",'M', LocalDate.of(1970,1,1),4L);
         Teacher teacher5=new Teacher("Saim","Abc",'M', LocalDate.of(1989,1,1),5L);
-        Teacher teacher6=new Teacher("Asya","Abc",'F', LocalDate.of(1990,1,1),6L);
+        Teacher teacher6=new Teacher("Asya","Abc",'F', LocalDate.of(1998,1,1),6L);
 
         teachers.addAll(Arrays.asList(teacher1,teacher2,teacher3,teacher4,teacher5,teacher6));
 
     }
     public static void main(String[] args) {
 
+        System.out.println(teachers);
+        ArrayList<Teacher> result=getTeacherNameStartWith("M");
+        System.out.println(result);
+
+        ArrayList<Teacher> female=getTeacherByGender('F');
+        System.out.println(female);
+
+        ArrayList<Teacher> small=getTeacherAgeSmaller30();
+        System.out.println(small);
+
+        ArrayList<Teacher> birthYear=getBirthYear1967();
+        System.out.println(birthYear);
+    }
+
+    public static ArrayList<Teacher> getBirthYear1967() {
+        ArrayList<Teacher> result=new ArrayList<>();
+        for (Teacher teacher : teachers) {
+            if (teacher.dateOfBirth.getYear()==1967){
+                result.add(teacher);
+            }
+        }
+        return result;
+    }
+
+    public static ArrayList<Teacher> getTeacherAgeSmaller30() {
+        ArrayList<Teacher > result=new ArrayList<>();
+        for (Teacher teacher : teachers) {
+            if (teacher.age<30){
+                result.add(teacher);
+            }
+        }
+        return result;
+    }
+
+    public static ArrayList<Teacher> getTeacherByGender(char gender) {
+        ArrayList<Teacher> result=new ArrayList<>();
+        for (Teacher teacher : teachers) {
+            if(teacher.gender==gender){
+                result.add(teacher);
+            }
+        }
+        return result;
+    }
+
+    public static ArrayList<Teacher> getTeacherNameStartWith(String prefix) {
+
+        ArrayList<Teacher> result=new ArrayList<>();
+        for (Teacher teacher : teachers) {
+            if(teacher.name.startsWith(prefix)){
+                result.add(teacher);
+            }
+        }
+        return result;
 
     }
+
+
 
 }
