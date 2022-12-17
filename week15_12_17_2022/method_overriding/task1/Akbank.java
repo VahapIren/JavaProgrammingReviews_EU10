a3001dbc14838d866c6c8079e2f50ad1ec2b9341
vahap iren <vahapiren@gmail.com>
2022-12-17 20:52:34 +0300

    week15: rewiew

---
+++ week15_12_17_2022/method_overriding/task1/Akbank.java
@@ -0,0 +1,16 @@
+package week15_12_17_2022.method_overriding.task1;
+
+public class Akbank extends Bank{
+    private int capital=10000;
+
+    public int getCapital(){
+        return capital;
+    }
+
+    // How we can get the class name?
+    //The answer is getClass().getSimpleName()
+
+    public void calculateTheInterest(){
+        System.out.println("Interest of the " + getClass().getSimpleName());
+    }
+}
