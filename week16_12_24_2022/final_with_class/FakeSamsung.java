382a6ea8a6d61492226cc01f9b15f0aae8e8e4e3
vahap iren <vahapiren@gmail.com>
2022-12-24 19:29:09 +0300

    week16: rewiew-final

---
+++ week16_12_24_2022/final_with_class/FakeSamsung.java
@@ -0,0 +1,9 @@
+package week16_12_24_2022.final_with_class;
+
+public class FakeSamsung //extends Samsung==>you cant extend the class which has final
+ {
+
+
+
+
+}
---
+++ week16_12_24_2022/final_with_methods/JuniorTester.java
@@ -0,0 +1,18 @@
+package week16_12_24_2022.final_with_methods;
+
+public class JuniorTester extends SeniorTester{
+    @Override
+    public void junior1() {
+        super.junior1();
+    }
+
+    @Override
+    public void junior2() {
+        super.junior2();
+    }
+
+    @Override
+    public void junior3() {
+        super.junior3();
+    }
+}
